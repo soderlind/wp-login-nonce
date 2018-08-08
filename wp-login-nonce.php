130ef34f3894cbc0b69d4dacbdd5c009ec623132
soderlind <per@soderlind.no>
2018-08-09 00:08:49 +0200

    add comments

--- wp-login-nonce.php
+++ wp-login-nonce.php
@@ -11,7 +11,7 @@
  * Plugin URI: https://github.com/soderlind/wp-login-nonce
  * GitHub Plugin URI: https://github.com/soderlind/wp-login-nonce
  * Description: Use a nonce to prevent Login Cross-Site Request Forgery (CSRF).
- * Version:     0.0.2
+ * Version:     0.0.3
  * Author:      Per Soderlind
  * Author URI:  https://soderlind.no
  * Text Domain: dss-login-nonce
@@ -25,7 +25,7 @@ add_action( 'login_form', __NAMESPACE__ . '\login_form_nonce_field' );
 add_filter( 'authenticate', __NAMESPACE__ . '\login_form_nonce_field_validate', 99, 3 );
 add_filter(
 	'nonce_user_logged_out', function ( $uid = 0, $action = '' ) {
-		if ( $action == 'loginsecurity' ) {
+		if ( $action == 'nonceauth' ) {
 			return 0;
 		}
 
@@ -33,26 +33,42 @@ add_filter(
 	}, 100, 2
 );
 
+/**
+ * Add a hidden nonce field to the login form.
+ *
+ * @return void
+ */
 function login_form_nonce_field() {
 	$token = bin2hex( openssl_random_pseudo_bytes( 16 ) );
 	setcookie( 'csrftoken', $token, time() + DAY_IN_SECONDS );
 
-	wp_nonce_field( 'nonceid_' . $token, 'loginsecurity' );
+	wp_nonce_field( 'nonceid_' . $token, 'nonceauth' );
 }
 
+/**
+ * Validate the nonce.
+ *
+ * If DUO Security 2FA is installed and activate, and this is the secondary auth by DUO,
+ * skip nonce validation.
+ *
+ * @param null|WP_User|WP_Error $user WP_User if the user is authenticated. WP_Error or null otherwise.
+ * @param string                $username Username or email address.
+ * @param string                $password User password.
+ * @return null|WP_User|WP_Error
+ */
 function login_form_nonce_field_validate( $user, $username, $password ) {
 	// Don't validate nonce when doing secondary auth by DUO
 	if ( function_exists( 'duo_auth_enabled' ) && duo_auth_enabled() && isset( $_POST['sig_response'] ) ) {
 		return $user;
 	}
 
-	if ( ! isset( $_REQUEST['loginsecurity'], $_COOKIE['csrftoken'] ) ) {
+	if ( ! isset( $_REQUEST['nonceauth'], $_COOKIE['csrftoken'] ) ) {
 		return $user;
 	}
-	$nonce = $_REQUEST['loginsecurity'];
+	$nonce = $_REQUEST['nonceauth'];
 	$token = $_COOKIE['csrftoken'];
 	if ( ! wp_verify_nonce( $nonce, 'nonceid_' . $token ) ) {
-		return new \WP_Error( 'login-nonce', '<strong>ERROR</strong>: Invalid nonce' );
+		return new \WP_Error( 'nonceauth', '<strong>ERROR</strong>: Invalid nonce' );
 	}
 	return $user;
 }
